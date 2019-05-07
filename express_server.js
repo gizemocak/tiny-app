e6ad7bbe170424971ddce5792592492937524e6e
gizemocak <gizem_ocak@outlook.com>
2019-05-07 19:13:14 +0000

    Add a route for /uls

--- express_server.js
+++ express_server.js
@@ -17,6 +17,13 @@ app.get('/uls.json', (req, res) => {
     res.json(urlDatabase)
 })
 
+app.get("/urls", (req, res) => {
+    let templateVars = {
+        urls: urlDatabase
+    };
+    res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
